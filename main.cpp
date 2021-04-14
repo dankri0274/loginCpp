e5c1c5dc0de2a632da642c727b2f84302310cc8e
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-04-14 09:43:16 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main() {
 	string username, passwd, passwdConf;
 	int tries = 0, maxTries = 5;
-restart:
+
+	restart:
+
 	cout << "Enter username: ";
 	cin >> username;
 	cout << "Register password: ";
@@ -12,31 +14,36 @@ restart:
 	cout << "Confirm password: ";
 	cin >> passwdConf;
 
+	if (username.length() < 6) {
+		cout << endl << "Minimum length for usernae is 6 digits!" << endl << endl;
+	}
+
 	if (passwd.length() < 8 || passwdConf.length() < 8) {
-		cout << "Minimum length for passwords is 8 digits" << endl;
+		cout << endl << "Minimum length for passwords is 8 digits" << endl << endl;
 		goto restart;
 	}
+
 	else {
 		cout << "___LOGIN___" << endl;
-    		string userLogin, passwdLogin;
+		string userLogin, passwdLogin;
 
-    		while (tries < maxTries) {
-	    		cout << "Username: ";
-	    		cin >> userLogin;
-	    		cout << "Password: ";
-	    		cin >> passwdLogin;
+		while (tries < maxTries) {
+			cout << "Username: " << endl;
+			cin >> userLogin;
+			cout << "Password: ";
+			cin >> passwdLogin;
 
-	    		if (userLogin == username && passwdLogin == passwdConf) {
-		    		tries++;
+			if (userLogin == username && passwdLogin == passwdConf) {
+				tries++;
 				cout << "Welcome " << username << endl;
 				break;
 			}
-	    		else {
-				cout << "Wrong login credentials entered, try again" << endl;
+			else {
+				cout << "Wrong login credentials entered, try again!" << endl;
 				tries++;
 			}
-		if (tries == maxTries) {
-			cout << "You have used all the tries, restart the program" << endl;
+			if (tries == maxTries) {
+				cout << "You have used all 5 tries, please restart the program" << endl;
 			}
 		}
 	}
